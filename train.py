5d0eab7a7f87715c3a83116230449573b2447924
Nicole Story <nicole7story@gmail.com>
2019-04-04 12:46:13 -0500

    Update to building feature matrix

--- train.py
+++ train.py
@@ -13,27 +13,52 @@ parent_directory = os.path.dirname(this_script)
 def build_feature_matrix(pssm_files, pssm_dir, rr_dir):
     """
     Builds a feature matrix based on PSSM and RR files
+    :return: List of dictionaries. Keys are feature numbers(0 - 199) and 'class'
     """
     feature_matrix = []
 
     for pssm_file in pssm_files:
         # For each training file, read in the PSSM matrix and the RR file
         pssm = utils.read_pssm(pssm_file, pssm_dir)
-        rr = utils.read_sequence(pssm_file.replace('.pssm', '.rr'), rr_dir)
-        for row_num in range(len(pssm)):
-            # For each amino acid in the PSSM, build a line for the feature matrix
-            feature = {'rr': rr[row_num]}
-            for row_offset in range(-2, 3):
-                if row_num + row_offset < 0:
-                    # We're at the top of the PSSM
-                    values = empty_row
-                elif row_num + row_offset >= len(pssm):
-                    # We're at the bottom of the PSSM
-                    values = empty_row
-                else:
-                    # We're somewhere in the middle
-                    values = pssm[row_num + row_offset]
-                for val_num, acid in enumerate(acids_list):
-                    feature[((row_offset + 2) * 20) + val_num] = values[acid]
-            feature_matrix.append(feature)
+        rr = utils.read_rr(pssm_file.replace('.pssm', '.rr'), rr_dir)
+        intermediate_matrix = build_small_matrix(pssm)
+
+        # For each i, j pair, make a row for the feature matrix
+        for i in range(len(intermediate_matrix)):
+            for j in range(i+5, len(intermediate_matrix)):
+                # Build row for feature matrix:
+                feature_row = {'class': 0}
+                feature_row.update(intermediate_matrix[i])
+                for feat_num in range(len(intermediate_matrix[j])):
+                    feature_row[100 + feat_num] = intermediate_matrix[j][feat_num]
+
+                # If (i, j) is in the .rr file, they're in contact
+                if (i, j) in rr:
+                    feature_row['class'] = 1
+                feature_matrix.append(feature_row)
     return feature_matrix
+
+
+def build_small_matrix(pssm):
+    """
+    Builds the 'small' intermediate matrix. Rows are 100 features from sliding window of size 5.
+    :return: List of dictionaries. Keys are feature numbers (0 - 99)
+    """
+    small_matrix = []
+    for row_num in range(len(pssm)):
+        # For each amino acid in the PSSM, build a line for the feature matrix
+        feature = {}
+        for row_offset in range(-2, 3):
+            if row_num + row_offset < 0:
+                # We're at the top of the PSSM
+                values = empty_row
+            elif row_num + row_offset >= len(pssm):
+                # We're at the bottom of the PSSM
+                values = empty_row
+            else:
+                # We're somewhere in the middle
+                values = pssm[row_num + row_offset]
+            for val_num, acid in enumerate(acids_list):
+                feature[((row_offset + 2) * 20) + val_num] = values[acid]
+        small_matrix.append(feature)
+    return small_matrix
