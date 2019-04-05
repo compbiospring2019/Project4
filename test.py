14b04044e8a833b97af7b8bda995c5591f2a4102
Nicole Story <nicole7story@gmail.com>
2019-04-05 16:44:25 -0500

    Removed saving to .rr

--- test.py
+++ test.py
@@ -15,23 +15,29 @@ parent_directory = os.path.dirname(this_script)
 # Set the directory for testing output
 rr_output_directory = parent_directory + "/testing-rr-output"
 
+
 def main(pssm_files, pssm_dir, rr_dir):
     test(pssm_files, pssm_dir, rr_dir)
+
 
 def test(pssm_files, pssm_dir, rr_dir):
     """
     Test the model. Generate contact probabilities and save them in .rr format sorted in descending order.
     """
     model = utils.read_model()
+    prob_list = []
     rr_rows = []
     # create directory for .rr files created by testing
+    '''
     try:
+        print('Making directory...')
         os.mkdir(rr_output_directory)
     except FileExistsError:
         print("Removing old .rr files...")
         old_rr_files = utils.read_directory_contents(rr_output_directory, '.rr')
         for old_rr_file in old_rr_files:
             os.remove(os.path.join(rr_output_directory, old_rr_file))
+    '''
     for pssm_file in pssm_files:
         # get the feature values of every pair
         test_matrix = build_test_matrix(pssm_file, pssm_dir)
@@ -39,19 +45,28 @@ def test(pssm_files, pssm_dir, rr_dir):
         rr = utils.read_rr(pssm_file.replace('.pssm', '.rr'), rr_dir)
         for pair in range(len(test_matrix)):
             contact_probability = calculate_contact_probability(model, test_matrix[pair])
+            prob_list.append(contact_probability)
             # create row
+    prob_list.sort(reverse=True)
+    print(len(prob_list))
+    print(prob_list[0])
+    print(prob_list[len(prob_list) - 1])
+
+    '''
             i, j = list(test_matrix[pair].keys())[0][0], list(test_matrix[pair].keys())[0][1]
             rr_row = [i, j, 0, 8, contact_probability]
             # rr_rows.append(" ".join([str(x) for x in rr_row]))
             rr_rows.append(rr_row)
         # sort rows
-        rr_rows.sort(key = lambda row: row[4], reverse = True)
+        rr_rows.sort(key=lambda row: row[4], reverse=True)
         # write to file
         file_name = os.path.join(rr_output_directory, pssm_file.replace('.pssm', '.rr'))
         with open(file_name, 'w') as file:
+            print('Writing {}'.format(file_name))
             # stringify all rows and put in file
             for row in rr_rows:
-                file.write(" ".join([str(x) for x in row]) + "\n")
+                file.write(" ".join([str(x) for x in row]) + "\n")'''
+
 
 def calculate_contact_probability(model, pair):
     """
@@ -60,6 +75,7 @@ def calculate_contact_probability(model, pair):
     feature_values = list(pair.values())[0]
     n = exp(model[0] + sum([model[i + 1] * feature_values[i] for i in range(200)]))
     return n / (1 + n)
+
 
 def build_test_matrix(pssm_file, pssm_dir):
     """
@@ -76,6 +92,7 @@ def build_test_matrix(pssm_file, pssm_dir):
             test_matrix.append(feature)
     return test_matrix
 
+
 def get_five(pssm, i):
     """
     :return: a list of the 100 feature values in the sliding window around i
@@ -89,6 +106,7 @@ def get_five(pssm, i):
         else:
             values.extend([pssm[i + row_offset][k] for k in acids_list])
     return values
+
 
 if __name__ == "__main()__":
     main()
